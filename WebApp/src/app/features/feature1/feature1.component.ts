61122024c6edff5977f6abfc4958af721b830ad9
PaulaCiocu <paulafarcas2002@yahoo.com>
2024-11-01 17:22:53 +0200

    search page + some page design

--- WebApp/src/app/features/feature1/feature1.component.ts
+++ WebApp/src/app/features/feature1/feature1.component.ts
@@ -1,18 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
 import { SingleImageUpload } from '../../shared/components/single-image-upload/single-image-upload'
+import { MatIcon } from '@angular/material/icon'
+import { MatLabel, MatSuffix } from '@angular/material/form-field'
+import { UserStore } from '../../core/stores/user.store'
+import { AsyncPipe, NgIf } from '@angular/common'
 
 @Component({
     selector: 'app-feature1',
     standalone: true,
-    imports: [SingleImageUpload],
+  imports: [SingleImageUpload, MatIcon, MatLabel, MatSuffix, AsyncPipe, NgIf],
     templateUrl: './feature1.component.html',
     styleUrl: './feature1.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Feature1Component {
+  private readonly userStore = inject(UserStore)
+  user$ = this.userStore.user$
+
     file: File | null = null
     onFileUpload(event: File | null) {
         this.file = event
         console.log(this.file)
     }
+
+
 }
--- WebApp/src/app/features/feature2/feature2.component.ts
+++ WebApp/src/app/features/feature2/feature2.component.ts
@@ -1,11 +1,28 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { SingleImageUpload } from '../../shared/components/single-image-upload/single-image-upload'
+import { MatIcon } from '@angular/material/icon'
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field'
+import { FormsModule } from '@angular/forms'
+import { MatInputModule } from '@angular/material/input'
 
 @Component({
     selector: 'app-feature2',
     standalone: true,
-    imports: [],
+  imports: [
+    SingleImageUpload,
+    MatIcon,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
+  ],
     templateUrl: './feature2.component.html',
     styleUrl: './feature2.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Feature2Component {}
+export class Feature2Component {
+  file: File | null = null
+  onFileUpload(event: File | null) {
+    this.file = event
+    console.log(this.file)
+  }
+}
