d2a0e27e1ba9561a16eceac4fe79e4aa2323a9b1
Robert Alex Barbu <robertalexbarbu@gmail.com>
2024-12-23 15:14:34 +0200

    add notifications part 1

--- flutter-project/skill_swap/lib/main.dart
+++ flutter-project/skill_swap/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:skill_swap/authentication/login.dart';
@@ -14,11 +15,35 @@ void main() async {
     Get.put(SkillsController());
   });
 
+  await setupFirebaseMessaging();
   //Get.put(AuthenticationController());
   //Get.put(SkillsController());
   
   runApp(const MyApp());
 }
+
+Future<void> setupFirebaseMessaging() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // Get the device's FCM token
+  String? token = await messaging.getToken();
+  print("FCM Token: $token");
+
+  // Handle foreground messages
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Received a message while in the foreground: ${message.notification?.title}');
+    // Show notification or update the app state as needed
+  });
+
+  // Handle background messages
+  FirebaseMessaging.onBackgroundMessage(backgroundMessageHandler);
+}
+
+Future<void> backgroundMessageHandler(RemoteMessage message) async {
+  print('Handling a background message: ${message.notification?.title}');
+  // Show notification or update the app state as needed
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
